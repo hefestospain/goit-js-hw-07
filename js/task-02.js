091828b4b804e9a67dd66ef219dcc5dc17c7e645
Oleh Cheka <olecheka11@gmail.com>
2021-08-04 23:23:48 +0200

    Update task-02.js
    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
     'Зелень',
     'Приправы',
   ];
+
+  //Cкрипт, который для каждого элемента массива ingredients создаст отдельный li, 
+  //после чего вставит все li за одну операцию в список ul.ingredients. 
+  //Для создания DOM-узлов используй document.createElement().
+
+  const listIngredients = document.querySelector("#ingredients");
+
+  for (const ingr of ingredients) {
+    //console.log(ingr);
+    let li = document.createElement("li");
+    li.textContent = ingr;
+
+    listIngredients.appendChild(li);
+  }
