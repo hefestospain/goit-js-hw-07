40f9c3515e3b1c92ae3f284ffc53fa96e66c1450
Oleh Cheka <olecheka11@gmail.com>
2021-08-20 23:08:26 +0200

    Update task-04.js
    task4 is modificated, done

--- js/task-04.js
+++ js/task-04.js
@@ -3,20 +3,18 @@
 //Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 
 let counterValue = 0;
+const valueElem = document.querySelector("#value");
 const increment = document.querySelector('[data-action="increment"]');
 const decrement = document.querySelector('[data-action="decrement"]');
-const valueElem = document.querySelector("#value");
 
-console.log(valueElem.textContent);
-console.log(increment)
-console.log(typeof counterValue)
+
 
 increment.addEventListener("click", () => {
-    valueElem.textContent = counterValue;
     console.log(counterValue +=1);
+    valueElem.textContent = counterValue;
 });
 
 decrement.addEventListener("click", () => {
-    valueElem.textContent = counterValue;
     console.log(counterValue -= 1);
+    valueElem.textContent = counterValue;
 });
